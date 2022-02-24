bafb5334ed327883121952dec2b3955c07504cf9
Cecilia Sjögren <cisci_13@hotmail.com>
2022-02-24 17:06:19 +0100

    Added mapping

---
+++ Exercies_14/Lms.Api/Lms.Data/Data/LmsMappings.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Lms.Core.Dto;
+using Lms.Core.Entities;
+
+namespace Lms.Data.Data
+{
+    public class LmsMappings : Profile
+    {
+        public LmsMappings()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Module, ModuleDto>().ReverseMap();
+
+        }
+    }
+}
