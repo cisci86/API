ecc288de418929b551441f1bcb93d1295dc02931
Cecilia Sjögren <cisci_13@hotmail.com>
2022-03-04 16:03:05 +0100

    Added PartialUpdateService

---
+++ Exercies_14/Lms.Api/Lms.Client/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Lms.Client.Service;
+
+namespace Lms.Client.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task<IApplicationBuilder> RunCrudServiceAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var provider = scope.ServiceProvider;
+
+                try
+                {
+                    var logger = provider.GetService<ILogger<Program>>();
+                    logger.LogInformation("Host Created.");
+
+                    await provider.GetService<IIntegrationService>().Run();
+                }
+                catch (Exception generalException)
+                {
+                    var log = provider.GetService<ILogger<Program>>();
+                    log.LogError(generalException, "An exception happened while running the integration service.");
+                }
+            }
+            return app;
+        }
+    }
+}
---
+++ Exercies_14/Lms.Api/Lms.Client/Service/PartialUpdateService.cs
@@ -0,0 +1,42 @@
+﻿using Lms.Client.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Lms.Client.Service
+{
+    public class PartialUpdateService : IIntegrationService
+    {
+        private static HttpClient _httpClient = new HttpClient();
+        public PartialUpdateService()
+        {
+            _httpClient.BaseAddress = new Uri("https://localhost:7045");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+        }
+        public async Task Run()
+        {
+           await PatchResource();
+        }
+        public async Task PatchResource()
+        {
+            var patchDoc = new JsonPatchDocument<CourseModifyDto>();
+            patchDoc.Replace(c => c.StartDate, DateTime.Now.AddDays(-3));
+
+            var serilizedChangedSet = JsonConvert.SerializeObject(patchDoc);
+
+            var request = new HttpRequestMessage(HttpMethod.Patch,
+                "api/courses/7");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Content = new StringContent(serilizedChangedSet);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json-patch+json");
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var updatedCourse = JsonConvert.DeserializeObject<CourseDto>(content);
+
+        }
+    }
+}
